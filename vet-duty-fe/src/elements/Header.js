2693d4e8455a1a4a7286555df7512449c6c708bd
Andre Moraes <andre.moraes@ab-inbev.com>
2023-12-15 15:46:57 -0300

    Finishing home

--- vet-duty-fe/src/elements/Header.js
+++ vet-duty-fe/src/elements/Header.js
@@ -1,11 +1,14 @@
 // Header.js
 import React from 'react';
+import Logo from '../public/images/logo.png';
+
 const Header = () => {
     return (
        <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Plantão Vet - Encontre ou Publique o seu</a>
+                <img src={Logo} alt="Logo" width={50}/>
+                <a class="navbar-brand" href="#">O Plantonista</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
--- vet-duty-fe/src/elements/SearchBox.js
+++ vet-duty-fe/src/elements/SearchBox.js
@@ -1,23 +1,76 @@
 import React, { useState } from 'react';
+import { Form, Row, Col, Button, Container } from 'react-bootstrap';
+import statesAndCities from '../utils/data/brazilcities.json';
+import speciality from '../utils/data/speciality.json';
 
 function SearchBox() {
   const [cityName, setCityName] = useState('');
+  const [selectedState, setSelectedState] = useState('');
+  const [selectedSpeciality, setSelectedSpeciality] = useState('');
+  const [cities, setCities] = useState([]);
 
-  const handleSearch = () => {
-    // Implemente a lógica de pesquisa aqui
-    alert(`Pesquisando plantões em: ${cityName}`);
+  const handleStateChange = (e) => {
+    const state = e.target.value;
+    setSelectedState(state);
+    setCities(state ? statesAndCities[state] : []);
+  };
+
+  const handleSpecialityChange = (e) => {
+    setSelectedSpeciality(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(`Pesquisando ${selectedSpeciality} em: ${cityName}, Estado: ${selectedState}`);
   };
 
   return (
-    <div className="search-box">
-      <input
-        type="text"
-        placeholder="Digite o nome da cidade"
-        value={cityName}
-        onChange={(e) => setCityName(e.target.value)}
-      />
-      <button onClick={handleSearch}>Pesquisar</button>
-    </div>
+    <Container className="d-flex align-items-center justify-content-center" style={{ height: "100vh" }}>
+      <Form fluid="md" onSubmit={handleSearch}>
+        <Form.Group as={Row} className="mb-3" controlId="formState">
+          <Form.Label column sm="2"></Form.Label>
+          <Col sm="10">
+            <Form.Select value={selectedState} onChange={handleStateChange}>
+              <option value="">Selecione um Estado</option>
+              {Object.keys(statesAndCities).map((state) => (
+                <option key={state} value={state}>{state}</option>
+              ))}
+            </Form.Select>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controlId="formCity">
+          <Form.Label column sm="2"></Form.Label>
+          <Col sm="10">
+            <Form.Select value={cityName} onChange={(e) => setCityName(e.target.value)} disabled={!selectedState}>
+              <option value="">Selecione uma Cidade</option>
+              {cities.map((city) => (
+                <option key={city} value={city}>{city}</option>
+              ))}
+            </Form.Select>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controlId="formSpeciality">
+          <Form.Label column sm="2"></Form.Label>
+          <Col sm="10">
+            <Form.Select value={selectedSpeciality} onChange={handleSpecialityChange}>
+              <option value="">Selecione uma Especialidade</option>
+              {speciality.especialidades.map((especialidade) => (
+                <option key={especialidade} value={especialidade}>{especialidade}</option>
+              ))}
+            </Form.Select>
+          </Col>
+        </Form.Group>
+
+        <Row>
+          <Form.Label column sm="2"></Form.Label>
+          <Col className="text-center">
+            <Button type="submit">Pesquisar</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
